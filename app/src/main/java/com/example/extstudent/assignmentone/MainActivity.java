7e10e932a671c2670eb04e84441d4c42c29ad484
elainefan99 <christmaspresents88@gmail.com>
2019-01-26 13:45:42 -0800

    AssignmentOne First Commit

---
+++ app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.extstudent.assignmentone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+ * CSE41246 - Android Java Fundamentals
+ * Fall 2019
+ * Assignment #1
+ * <Fan>, <Elaine>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
