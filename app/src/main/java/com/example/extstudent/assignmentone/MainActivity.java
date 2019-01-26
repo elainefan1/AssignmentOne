4fc7d339860fa6e842598b5944b3481f7f3ff2e7
elainefan99 <christmaspresents88@gmail.com>
2019-01-26 14:03:56 -0800

    AssignmentOne First Commit

--- app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.extstudent.assignmentone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 /*
  * CSE41246 - Android Java Fundamentals
  * Fall 2019
@@ -9,10 +12,29 @@ import android.os.Bundle;
  * <Fan>, <Elaine>
  */
 public class MainActivity extends AppCompatActivity {
+    private String firstName;
+    private String lastName;
 
+    private TextView firstNameTextView = null;
+    private TextView lastNameTextView = null;
+    private Button submitButton = null;
+
+    private TextView resultText = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set Variables.
+        this.firstNameTextView = (TextView)findViewById(R.id.editTextFirstName);
+        this.lastNameTextView = (TextView)findViewById(R.id.editTextLastName);
+        this.resultText = (TextView) findViewById(R.id.textViewResult);
+
+        this.submitButton = (Button)findViewById(R.id.submitButton);
+        this.submitButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void OnClick (View v) {
+
+            }
+        }
     }
 }
