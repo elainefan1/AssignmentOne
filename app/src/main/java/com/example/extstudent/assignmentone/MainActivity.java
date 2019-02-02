f6dc3265ae2299f2b9a08902fd8868baf9c763cc
elainefan99 <christmaspresents88@gmail.com>
2019-02-02 13:17:00 -0800

    AssignmentOnecommit

--- app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/extstudent/assignmentone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.extstudent.assignmentone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView firstNameTextView = null;
     private TextView lastNameTextView = null;
+    private TextView resultTextView = null;
     private Button submitButton = null;
 
     private TextView resultText = null;
@@ -27,14 +29,41 @@ public class MainActivity extends AppCompatActivity {
         //Set Variables.
         this.firstNameTextView = (TextView)findViewById(R.id.editTextFirstName);
         this.lastNameTextView = (TextView)findViewById(R.id.editTextLastName);
-        this.resultText = (TextView) findViewById(R.id.textViewResult);
-
+        //this.resultText = (TextView) findViewById(R.id.textViewResult);
+        this.resultTextView = (TextView)findViewById(R.id.textViewResult);
         this.submitButton = (Button)findViewById(R.id.submitButton);
+
+
+
+
         this.submitButton.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void OnClick (View v) {
+            public void onClick(View v) {
 
+                setUserInfo();
+//
+//                Log.d("Last Name" , lastName);
+                showResults();
             }
+
+        } );
+    }
+
+    private void setUserInfo() {
+        if(this.firstNameTextView != null) {
+            this.firstName = this.firstNameTextView.getText().toString();
+            Log.i("First Name:", firstName);
+            firstName = this.firstNameTextView.getText().toString();
+            lastName = this.lastNameTextView.getText().toString();
         }
     }
+
+    private void showResults() {
+
+
+        String resultStr = " Hi, nice to meet you, " + firstName + " " + lastName;
+
+        this.resultTextView.setText(resultStr);
+    }
+
 }
